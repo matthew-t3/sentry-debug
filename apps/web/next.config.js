5c484c1c01e1f361556b010deccc2a6600f71240
Matthew Torres <matthew.torres@terminal3.io>
2024-09-17 21:50:55 +0800

    Remove silent flag

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -18,9 +18,6 @@ module.exports = withSentryConfig(
     org: "throwawaysentry",
     project: "sentry-debug",
 
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
